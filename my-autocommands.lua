28e76e7e4cb25103c929c6bfea18b3f050ae0d7d
Milton Carlos Katoo <mckatoo@gmail.com>
2024-10-21 13:05:21 -0300

    using plenary for async creation configurations

--- my-autocommands.lua
+++ my-autocommands.lua
@@ -1,10 +1,16 @@
 ---@diagnostic disable: undefined-global
 
-function xmake_config_generate()
-  vim.cmd "silent !xmake project --kind=compile_commands --lsp=clang -y"
-  vim.cmd "silent !xmake project --kind=cmakelists -y"
+local async = require('plenary.async')
+xmake_config_generate = async.void(function()
+  print("creating...")
+  async.void(function()
+    vim.cmd "silent !xmake project --kind=compile_commands --lsp=clang -y"
+  end)()
+  async.void(function()
+    vim.cmd "silent !xmake project --kind=cmakelists -y"
+  end)()
   print("xmake config files created!")
-end
+end)
 
 lvim.autocommands = {
   {
@@ -26,7 +32,6 @@ lvim.autocommands = {
     {
       pattern = { "xmake.lua" },
       command = "lua xmake_config_generate()"
-      -- command = "!xmake project --kind=compile_commands --lsp=clang"
     }
   }
 }
