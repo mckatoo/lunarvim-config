fc421680094cbb83a2c9d904daed736e33561590
Milton Carlos Katoo <mckatoo@gmail.com>
2024-11-03 18:03:27 -0300

    used master branch for vim-visual-multi

--- my-plugins.lua
+++ my-plugins.lua
@@ -1,21 +1,8 @@
 ---@diagnostic disable: undefined-global
 vim.list_extend(lvim.plugins, {
   {
-    'mg979/vim-visual-multi',
-    init = function()
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'visual_multi_start',
-        callback = function()
-          pcall(vim.keymap.del, 'i', '<BS>', { buffer = 0 })
-        end,
-      })
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'visual_multi_exit',
-        callback = function()
-          require('nvim-autopairs').force_attach()
-        end,
-      })
-    end,
+    "mg979/vim-visual-multi",
+    branch = "master"
   },
   {
     "kylechui/nvim-surround",
