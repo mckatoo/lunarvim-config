b109119abbe44bc11a0d1ffbfd02b659b150d1d2
Milton Carlos Katoo <mckatoo@gmail.com>
2024-11-10 22:43:20 -0300

    toogle comma and semmicolon

--- utils.lua
+++ utils.lua
@@ -7,7 +7,7 @@ function xmake_config_generate()
   print("xmake config files created!")
 end
 
-function show_diagnostics_info ()
+function show_diagnostics_info()
   local opts = {
     focusable = false,
     close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
@@ -22,3 +22,28 @@ function show_diagnostics_info ()
   end
 end
 
+local Symbol = {
+  comma = ',',
+  semmicolon = ';'
+}
+
+local function toggle(comma_or_semmicolon)
+  local line = vim.api.nvim_get_current_line()
+  local cmd = comma_or_semmicolon .. "$"
+  if line:match(cmd) then
+    print('tem virgula')
+    line = line:gsub(cmd, "")
+  else
+    print('NÃO tem virgula')
+    line = line .. comma_or_semmicolon
+  end
+  vim.api.nvim_set_current_line(line)
+end
+
+function Toggle_comma()
+  toggle(Symbol.comma)
+end
+
+function Toggle_semicolon()
+  toggle(Symbol.semmicolon)
+end
