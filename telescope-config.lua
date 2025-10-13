49488dfbdd7ef269174308edde89e715e6fe5f0f
Milton Carlos Katoo <mckatoo@gmail.com>
2025-10-13 18:56:10 -0300

    telescope config for monorepo

--- telescope-config.lua
+++ telescope-config.lua
@@ -1,10 +1,11 @@
 local telescope = require('telescope')
 
-
 telescope.setup {
   pickers = {
     colorscheme = {
       enable_preview = true,
     }
-  }
+  },
+  cwd = vim.fn.getcwd(), -- Usa o diretório atual em vez de seguir o arquivo
 }
+
